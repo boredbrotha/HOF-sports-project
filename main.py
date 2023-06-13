521a2bef2a65e4435acea2f48ec0fe1121ed9f40
Asahd <soatman06@gmail.com>
2023-06-13 09:55:32 -0500

    Added function functionality =)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import time
 import pandas
 
+"""
 username = "sallyevesilber"
 
 getUserID = "https://instagram-api-20231.p.rapidapi.com/api/get_user_id/{}".format(username)
@@ -34,10 +35,10 @@ for i in range(1,timesToIterate):
     time.sleep(30)
     
 print("done")
+"""
 
+athList = ["billyguzzo_", "mrrelentess", "cadenowik", "vin_cognetta" ,"jordan_waterhouse03"]
 
-
-print(followerList)
 
 def outputFollowers(dataFrame, accTP):
     """
@@ -50,4 +51,53 @@ def outputFollowers(dataFrame, accTP):
     Output: A spreadsheet!
     """
 
+    headers = {
+	"X-RapidAPI-Key": "cdbbb8b397msh7ad25518cbc088ep1bba24jsn2e54fcaaac66",
+	"X-RapidAPI-Host": "instagram-api-20231.p.rapidapi.com"
+    }
+
+    for i in accTP:
+        username = i
+        getUserID = "https://instagram-api-20231.p.rapidapi.com/api/get_user_id/{}".format(username)
+
+        r0 = requests.get(getUserID, headers=headers) #Get userID from username
+
+        userID=r0.json()['data']['id']
+
+        fCount = r0.json()['data']['followers']
+
+        #Now we'll check if this userID is private
+        getUserInfo = "https://instagram-api-20231.p.rapidapi.com/api/get_user_info/{}".format(userID)
+
+        r1 = requests.get(getUserInfo, headers=headers)
+
+        isPrivate = r1.json()['data']['is_private']
+
+        if isPrivate == 'false': #If isPrivate is false, we get all of the usernames
+            
+            getFollowers = "https://instagram-api-20231.p.rapidapi.com/api/user_followers/{}".format(userID)
+            timesToIterate = fCount%100
+            followerList = []
+            querystring = None
+            for i in range(1,timesToIterate):
+
+                r2 = requests.get(getFollowers, headers=headers,params= querystring)
+
+                querystring = {"max_id":"{}".format(r2.json()['data']['next_max_id'])}
+
+                for i in range(len(r2.json()['data']['users'])):
+
+                    followerList.append(r2.json()['data']['users'][i]['username'])
+
+                time.sleep(30)
     
+
+        else:
+            pass
+            #ADD LATER: make fCount a new row for the corresponding name
+            
+    print("done")
+
+
+
+
