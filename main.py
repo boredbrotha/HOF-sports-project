67290ace2dacdda32b72163f655d48b8b6cacf90
Asahd <soatman06@gmail.com>
2023-06-15 12:53:50 -0500

    more bugfixing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import pandas as pd
+from sys import exit
 
 
 def outputFollowers(athDict,accTP):
@@ -22,6 +23,8 @@ def outputFollowers(athDict,accTP):
     for cAthlete in accTP:
         username = cAthlete
         getUserID = "https://instagram-api-20231.p.rapidapi.com/api/get_user_id/{}".format(username)
+
+        print("Debug: We're currently working on: ",cAthlete)
 
         r0 = requests.get(getUserID, headers=headers) #Get userID from username
 
@@ -45,29 +48,39 @@ def outputFollowers(athDict,accTP):
             if fCount%100 == 0:
                 timesToIterate = fCount/100
             else:
-                timesToIterate = fCount//100 +3
+                timesToIterate = fCount//100 +2
 
             querystring = None
 
-            last_one = False
-            for i in range(1,timesToIterate):
 
-                r2 = requests.get(getFollowers, headers=headers,params= querystring)
-                if('next_max_id' in r2.json()['data']):
-                    querystring = {"max_id":"{}".format(r2.json()['data']['next_max_id'])}
-                else: 
-                    querystring = None
-                    last_one = True
+            while True:
+                try:
+                    r2 = requests.get(getFollowers, headers=headers,params= querystring)
                     
+                    if r2.json()['data']['big_list'] == False:
+                        for i in range(len(r2.json()['data']['users'])):
+                            athDict[cAthlete].append(r2.json()['data']['users'][i]['username'])
+                        break
 
-                for i in range(len(r2.json()['data']['users'])):
+                    if('next_max_id' in r2.json()['data']):
+                        querystring = {"max_id":"{}".format(r2.json()['data']['next_max_id'])}
+                        
+                    print("Debug: Current ID: ",r2.json()['data']['next_max_id'])
 
-                    athDict[cAthlete].append(r2.json()['data']['users'][i]['username'])
+                    for i in range(len(r2.json()['data']['users'])):
+
+                        athDict[cAthlete].append(r2.json()['data']['users'][i]['username'])
+
+                    print("Debug: appended to dictionary!")
 
 
-                #time.sleep(15)
-                if last_one ==True:
-                    break
+                    #time.sleep(15)
+
+                    
+                except:
+                    print("An error has occurred. Here's what the API returns:")
+                    print(r2.json())
+                    exit()
 
         else:
             pass
@@ -83,79 +96,12 @@ def main():
     'cadenowik': [],
     'vin_cognetta': [],
     'jordan_waterhouse03': [],
-    'owen_fallon': [],
-    'nhalnbk7': [],
-    'vzelisko': [],
-    'josh_ilo': [],
-    'dcunningham._': [],
-    'abrownlee21': [],
-    'claydenstorff': [],
-    'alexdobbins14': [],
-    'higlejonathon': [],
-    'danieldesantis4': [],
-    'oliverkovass': [],
-    'jimmy.stines': [],
-    'alex.stackhouse': [],
-    'treyfabrocini': [],
-    'mason.marchinsky': [],
-    '_joshstegich': [],
-    'lariviere.christian': [],
-    '_sean_carr': [],
-    'michael_ballenger': [],
-    'jonadamson11': [],
-    'hdirico21': [],
-    'nimer_26': [],
-    'cadecormier7': [],
-    'chaaunce': [],
-    'jonathanbearden2': [],
-    'eddiedreher': [],
-    'willsolis27': [],
-    'collin.j.murphy': [],
-    'matthewwiley_': [],
-    'charliep_22_': [],
-    'andrew_brown_37': [],
-    'colinluse': [],
-    'michaelmagruire48': [],
-    '_jeffmoore': [],
-    'nj.austria': [],
-    'willisgauze': [],
-    'cole_graney36': [],
-    'caseycampbell00': [],
-    'connor_kratzert': [],
-    'cburzynski55': [],
-    'thegrandcanyon_56': [],
-    'matthewrussell20': [],
-    'samsecrest': [],
-    'gmcclung_': [],
-    'kevin_stone123456': [],
-    'brianstone7587': [],
-    'austin.smith7.3': [],
-    'jacksonwalsh20': [],
-    'h.slack_': [],
-    'willsforman': [],
-    'alex_sket21': [],
-    't.dunn_': [],
-    'trevor.goodrich': [],
-    'prestona_73': [],
-    'kadyn0101': [],
-    'loganyater3': [],
-    '__jwill3': [],
-    'evangelo_24': [],
-    'preston.fisk': [],
-    'jthallett15': [],
-    'casper_rublowsky': [],
-    'mpmead25': [],
-    'liam.mendham': [],
-    'davidsonojump': [],
-    'shawn.ugbana': [],
-    'jackp_dee': [],
-    'ryanlallly': []
 }
     
     outputFollowers(athleteDict,athleteDict.keys())
 
     data = pd.DataFrame.from_dict(athleteDict, orient= 'index')
-
+    data = data.transpose()
     data.to_excel("output.xlsx")
     
 main()
